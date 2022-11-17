ad6d890e33965f5e6ea0316f28e98f88a24b28e4
Mittal-web <sauravmittal988@gmail.com>
2022-11-17 14:34:50 +0530

    Single Page Application Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import Contact from './ContactComponent'
+import DishDetail from './DishDetailComponent'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends Component {
@@ -31,12 +32,20 @@ class Main extends Component {
                 />
             )
         }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                />
+            )
+        }
         return (
             <div className="App">
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        />
                 </Switch>
