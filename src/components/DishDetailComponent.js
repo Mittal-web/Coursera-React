ba8cf2d56770474ec85df2554e15f788cdcdca8d
Mittal-web <sauravmittal988@gmail.com>
2022-12-08 22:03:49 +0530

    Fetch From Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form'
 import { Loading } from './LoadingComponent'
-
+import { baseURL } from '../shared/baseURL';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
 const minLength = (len) => (val) => (val) && (val.length >= len)
@@ -114,7 +114,7 @@ function RenderDish({ dish }) {
     console.log('renderDish Component')
     return (
       <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImg width="100%" src={baseURL + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
